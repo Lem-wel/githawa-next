5ab8caf441d9fe45269c4dce748e98e3cdbf5790
Lem-wel <lemuelbanzuela.25@gmail.com>
2026-03-07 21:50:25 +0800

    fixing UI for mobile

--- src/app/chat-embed/page.tsx
+++ src/app/chat-embed/page.tsx
@@ -5,7 +5,7 @@ import GinhawaWixChat from "@/components/GinhawaWixChat";
 export default function ChatEmbedPage() {
   return (
     <div style={{ width: "100%", height: "700px", margin: 0 }}>
-      <GinhawaWixChat forceOpen />
+      <GinhawaWixChat />
     </div>
   );
 }
